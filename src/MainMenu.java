c61b08571f0e627cef4e5daf0f92beb4597c4da6
mezyn <84513662+mezyn@users.noreply.github.com>
2021-09-16 16:11:53 +0200

    Commiting the main menu

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,29 +1,43 @@
 import java.util.Scanner;
 
 public class MainMenu {
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
         Scanner OptionScan = new Scanner(System.in);
+
         System.out.println(
                 "Main Menu: Please chose among the options below \n" +
-                "0. Close System.\n" +
-                "1. Open Item Options.\n" +
-                "2. Open Review options.\n" +
-                "3. Open Transaction History Options.\n" +
-                "Type any Option Number:");
+                        "0. Close System.\n" +
+                        "1. Open Item Options.\n" +
+                        "2. Open Review options.\n" +
+                        "3. Open Transaction History Options.\n\n" +
+                        "Type any Option Number:");
 
-        int counter = 0;
-        do {
-            int option = OptionScan.nextInt();
+        int option = OptionScan.nextInt();
+
+        while (option != 0 && option != 1 && option != 2 && option != 3) {
+
+            System.out.println("Type an alternative between 0 and 3.\nType any option number: ");
+            option = OptionScan.nextInt();
+        }
 
             switch (option) {
-                case 0 : System.out.println("Closed"); // create method
-                case 1 : Item.itemOption();
-                case 2 : Review.ReviewMenu();
-                case 3 : System.out.println("Transaction History");
-                default : System.out.println("Please enter a valid option");
+                case 0:
+                    System.out.println("Closed");
+                    break;// create method
+                case 1:
+                    Item.itemOption();
+                    break;
+                case 2:
+                    Review.ReviewMenu();
+                    break;
+                case 3:
+                    System.out.println("Transaction History");
+                    break;
+
             }
-        } while (counter >= 5) ;
-        OptionScan.close();
+
+            OptionScan.close();
+
         }
     }
