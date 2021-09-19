a09d0f8cca4df82767c2844dbe16171d7b446855
mezyn <84513662+mezyn@users.noreply.github.com>
2021-09-19 22:44:49 +0200

    Commiting Item

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -6,18 +6,18 @@ public class MainMenu {
         Scanner OptionScan = new Scanner(System.in);
 
         System.out.println(
-                "Main Menu: Please chose among the options below \n" +
-                        "0. Close System.\n" +
-                        "1. Open Item Options.\n" +
+                "Main Menu: Please choose among the options below. \n\n" +
+                        "0. Close system.\n" +
+                        "1. Open Item options.\n" +
                         "2. Open Review options.\n" +
-                        "3. Open Transaction History Options.\n\n" +
-                        "Type any Option Number:");
+                        "3. Open Transaction History options.\n\n" +
+                        "Type an option number:");
 
         int option = OptionScan.nextInt();
 
         while (option != 0 && option != 1 && option != 2 && option != 3) {
 
-            System.out.println("Type an alternative between 0 and 3.\nType any option number: ");
+            System.out.println("Type an option number between 0 and 3.\nType any option number: ");
             option = OptionScan.nextInt();
         }
 
