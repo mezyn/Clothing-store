a479e785962197c2771ac2eee575d5791d305a6d
mezyn <84513662+mezyn@users.noreply.github.com>
2021-09-23 15:28:13 +0200

    createItems

---
+++ src/CreateItems.java
@@ -0,0 +1,10 @@
+/*I want to create items in my system so that I can sell them in my system.
+Each item has an identified (ID), name and a price per unit value.
+Once created, the ID of an item cannot change and two items cannot have the same ID.
+The creation of items is prompted in the “Items options” menu.
+In order to create an item, the user must input: (i) the item ID, (ii) the item name and (iii) the item price.
+When the item is created successfully, the system should print the message: "Item <ID> was registered successfully." */
+
+public class CreateItems {
+
+}
