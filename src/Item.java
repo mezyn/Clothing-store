7d95dc274d1416c0280295de8d9bd801c9082f4a
mezyn <84513662+mezyn@users.noreply.github.com>
2021-09-29 17:21:32 +0200

    Working on 2.3

--- src/Item.java
+++ src/Item.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
 
 public class Item {
 
     //attributes of Item class
-    private String ID; //I'll put this as String for now /Mijin
-    String name;
-    double pricePerUnit;
+     private final String ID; //I'll put this as String for now
+     private String name;
+     private double pricePerUnit;
 
 
     //constructor
@@ -16,8 +17,13 @@ public class Item {
 
     }
 
+    //ArrayList for items
+    //Declare in the attribute, initialize in the method
+    public static ArrayList<Item> itemList = new ArrayList<>();
+
+
     //method for creating items
-    public static Item createItem(){
+    public static void createItem(){
 
         String ID = UserInput.readLine("Type ID of item: ");
         String name = UserInput.readLine("Type name of item: ");
@@ -26,12 +32,47 @@ public class Item {
         Item newItem = new Item(ID, name, pricePerUnit);
         System.out.println(name + " " + ID + " was registered successfully.");
 
-        return newItem;
+        itemList.add(newItem);
 
         //Question to TA: where should we close the scanner?
 
-
     }
+
+    public void modifyItem() {
+
+        String itemToModify = UserInput.readLine("Type ID to change item name: ");
+
+        while (!itemList.contains(itemToModify)) {
+
+            System.out.println("There is no item called " + itemToModify + " in the list.");
+        }
+
+        String newName = UserInput.readLine("Type a new name for the item: ");
+        //Working on it
+
+        }
+
+
+
+
+// Getters and setters
+    public String getItemName() {
+        return name;
+    }
+
+    public double getItemPricePerUnit() {
+        return pricePerUnit;
+    }
+
+    public void setItemName(String name) {
+        this.name = name;
+    }
+
+    public void setItemPricePerUnit(double pricePerUnit) {
+        this.pricePerUnit = pricePerUnit;
+    }
+
+
 
 }
 
--- src/ItemMenu.java
+++ src/ItemMenu.java
@@ -1,4 +1,7 @@
+//System.lineSeparator
+
 public class ItemMenu {
+
 
     //item menu
     public static void itemOption() {
@@ -32,8 +35,10 @@ public class ItemMenu {
                 break;
             case 4 : System.out.println("5");
                 break;
+            //5. Update an item’s name.
             case 5 : System.out.println("6");
                 break;
+                //6. Update an item’s price.
             case 6 : System.out.println("7");
                 break;
         }
