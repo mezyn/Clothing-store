aeef062c7457f255ae795989eaed5528d7568053
Carl <carldt.cd@gmail.com>
2021-09-30 14:46:38 +0200

    Change

--- src/CreateReview.java
+++ src/CreateReview.java
@@ -15,7 +15,7 @@ public class CreateReview { // we need to import the arraylist from item to be a
 
     public static CreateReview reviewer() {
 
-        System.out.println("To create a review for a item please enter ID of item:");
+        System.out.println("To create a review for a item please enter ID of the item:");
 
             String Id = UserInput.readLine("ID number: "); // I guess here we want to add an attribute for the ID: X ? ASK TA
             String notation = UserInput.readLine("What did you like or dislike about this item?: ");
@@ -38,6 +38,6 @@ public class CreateReview { // we need to import the arraylist from item to be a
 
 }
 
-/* I think that we need to add the double grade and String notation for an item here aswell.
+/* I think that we need to add the double grade and String notation for an item here as well.
     So that we can use setter in review to change the attributes in this file, so we will just leave them empty
      until we set the attributes from createReview file. Not sure if this is the best way? // Carl */
